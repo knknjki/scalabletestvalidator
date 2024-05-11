32fe0640fb01a9f7a1ec00d4fbe268d9833e3ce9
knknjki <msjcadgamer2003@hotmail.com>
2024-05-11 18:17:36 +0700

    update sdk commit

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-function findKthLargest(nums, k) {
-  nums.sort((a, b) => b - a);
-  return nums[k - 1];
+function lengthOfLongestSubstring(s) {
+  if (s.length === 0) return 0;
+  const map = new Map();
+  let maxLength = 0;
+  let left = 0;
+  for (let right = 0; right < s.length; right++) {
+    if (map.has(s[right])) left = Math.max(left, map.get(s[right]) + 1);
+    map.set(s[right], right);
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+  return maxLength;
 }
