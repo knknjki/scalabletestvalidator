93642d048d99d8d05951c32b1977fbd739201275
knknjki <msjcadgamer2003@hotmail.com>
2024-05-15 03:56:17 +0700

    demo commit

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function removeDuplicates(arr) {
-  return [...new Set(arr)];
-}
+const insertionSort = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = key;
+  }
+  return arr;
+};
